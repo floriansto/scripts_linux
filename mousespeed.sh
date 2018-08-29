f82fec89b6ecb4ace3fb5ebc402f8e6e44bbc39e
Florian Stockburger <93-orangen@web.de>
2018-08-30 01:25:20 +0200

    add mousespeed and setup_wifi script, set permissions to 755

---
+++ mousespeed.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+xinput --set-float-prop "Razer Razer DeathAdder" 'Device Accel Constant Deceleration' 1.8 &
+xinput --set-prop "Razer Razer DeathAdder" 'Device Accel Velocity Scaling' 1
+
+
