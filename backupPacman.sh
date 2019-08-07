d708b2a9ae5097b362a9659494ca9d71349bb2d5
Florian Stockburger <florian.stockburger@web.de>
2019-08-08 01:27:42 +0200

    Add scipt to backup all installed pacman packages

---
+++ backupPacman.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+backupDir="$HOME/Backup"
+backupName="pacman_packages"
+timeStamp=$(date +%Y-%m-%d_%H.%M.%S)
+maxBackups=2
+
+existingBackups=$(ls -d $backupDir/* | grep $backupName)
+numBackups=$(echo $existingBackups | wc -w)
+if [[ $numBackups -ge $maxBackups ]]; then
+    numToDel=$((numBackups-maxBackups+1))
+    toDel=$(echo $existingBackups | cut -f-$numToDel -d " ")
+    rm $toDel
+fi
+
+pacman -Qeq > ${backupDir}/${backupName}_${timeStamp}
