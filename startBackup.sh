8164d11fe958a616de65840349ace6d2b6e9d664
Florian Stockburger <florian.stockburger@web.de>
2021-03-20 22:20:36 +0100

    Start and stop ssh server

--- startBackup.sh
+++ startBackup.sh
@@ -6,6 +6,9 @@ SSH_PORT=$2
 DEVICE=$1
 DBT="/root/data-backup-tool"
 
+# Start ssh server
+systemctl start sshd.service
+
 # Start rsnapshot from backup server
 ssh -p $SSH_PORT $BACKUP_USER@$BACKUP_HOST "python3 $DBT/src/main.py $DBT/config/$1.yaml -s -u $BACKUP_USER -h $1 -p $SSH_PORT"
 
@@ -16,3 +19,7 @@ if [[ $RET_VAL -eq 0 ]]; then
 else
     MESSAGE="Error: Backup finished with code $RET_VAL please investigate"
 fi
+
+# Stop ssh server
+systemctl stop sshd.service
+
