bd579a872267308cb3197bcad51cc859f2fb8886
Florian Stockburger <stockburger.florian@gmail.com>
2022-03-28 23:13:50 +0200

    Add cmd options for backup name and excludefile

--- startBackup.sh
+++ startBackup.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+function usage() {
+  echo "Usage $0 OPTIONS PATHS"
+  echo ""
+  echo "Wrapper script to backup your system using borgbackup"
+  echo "A file called env has to be in the same directory"
+  echo ""
+  echo "Required arguments"
+  echo " -n,--name   Name of the backup (usually the hostname)"
+  echo ""
+  echo "Optional arguments"
+  echo " -e,--exclude  Path to the excludefile (optional):"
+  echo ""
+  echo "The name has to be the first argument and is required"
+  echo "The paths are a space separated list of directories that you"
+  echo "want to backup"
+}
+
 SCRIPTDIR=$(dirname $0)
 ENV="$SCRIPTDIR/./env"
 
@@ -12,6 +29,8 @@ if [[ ! -e $ENV ]]; then
   echo "export GOTIFY_URL=''"
   echo "export GOTIFY_TOKEN=''"
   echo "export EXCLUDE_FILE=''"
+  echo ""
+  usage
   exit 1
 fi
 
@@ -20,7 +39,6 @@ GOTIFY_CALL="$GOTIFY_URL/message?token=$GOTIFY_TOKEN"
 
 if [ -z ${GOTIFY_URL+x} ]; then echo "\$GOTIFY_URL is unset";fi
 if [ -z ${GOTIFY_TOKEN+x} ]; then echo "\$GOTIFY_TOKEN is unset";fi
-
 if [ -z ${GOTIFY_URL+x} ] || [ -z ${GOTIFY_TOKEN+x} ]; then exit 1;fi
 
 EXCLUDE_STR=""
@@ -30,7 +48,31 @@ if [ ! -z ${EXCLUDE_FILE+x} ]; then
   fi
 fi
 
-HOSTNAME=$(hostname)
+name_provided=false
+while [[ ! -z "$1" ]]; do
+  echo "Begin loop with $1"
+  if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    usage
+    exit 0
+  elif [[ "$1" == "-n" || "$1" == "--name" ]]; then
+    echo $1
+    HOSTNAME="$2"
+    name_provided=true
+    shift
+    shift
+  elif [[ "$1" == "-e" || "$1" == "--exclude" ]]; then
+    EXCLUDE_STR="--exclude-from $2"
+    echo $2
+    shift
+    shift
+  else
+    if [[ $name_provided == true ]]; then
+      break
+    fi
+    usage
+    exit 1
+  fi
+done
 
 # Setting this, so the repo does not need to be given on the commandline:
 export BORG_REPO=ssh://root@backup:5176/mnt/backup/$HOSTNAME
