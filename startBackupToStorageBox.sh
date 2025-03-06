5cf53efe37c5de6bd0f758b76c21686dd91bc314
Florian Stockburger <stockburger.florian@gmail.com>
2025-03-06 09:02:32 +0100

    Add script to backup files from the server to the storage box

---
+++ startBackupToStorageBox.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SCRIPTDIR=$(dirname $0)
+USER=$1
+
+$SCRIPTDIR/startBackup.sh -p $SCRIPTDIR/borg_passphrase_backup_box1_dell-t20.txt -n $(hostname) -r ssh://$USER-sub1@$USER.your-storagebox.de:23/home/dell-t20 /etc /root /var/spool
+$SCRIPTDIR/startBackup.sh -p $SCRIPTDIR/borg_passphrase_backup_box1_flo-data.txt -n $(hostname) -r ssh://$USER-sub1@$USER.your-storagebox.de:23/home/flo-data /tank/subvol-100-nas-flo
+
+$SCRIPTDIR/checkBackups.sh -p $SCRIPTDIR/borg_passphrase_backup_box1_dell-t20.txt -r ssh://$USER-sub1@$USER.your-storagebox.de:23/home/dell-t20
+$SCRIPTDIR/checkBackups.sh -p $SCRIPTDIR/borg_passphrase_backup_box1_flo-data.txt -r ssh://$USER-sub1@$USER.your-storagebox.de:23/home/flo-data
+
