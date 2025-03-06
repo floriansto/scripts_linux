b9cc1df746ad3d117d234f8068e044a26dc167a2
Florian Stockburger <stockburger.florian@gmail.com>
2025-03-06 09:08:23 +0100

    Fix backup name

--- startBackupToStorageBox.sh
+++ startBackupToStorageBox.sh
@@ -4,7 +4,7 @@ SCRIPTDIR=$(dirname $0)
 USER=$1
 
 $SCRIPTDIR/startBackup.sh -p $SCRIPTDIR/borg_passphrase_backup_box1_dell-t20.txt -n $(hostname) -r ssh://$USER-sub1@$USER.your-storagebox.de:23/home/dell-t20 /etc /root /var/spool
-$SCRIPTDIR/startBackup.sh -p $SCRIPTDIR/borg_passphrase_backup_box1_flo-data.txt -n $(hostname) -r ssh://$USER-sub1@$USER.your-storagebox.de:23/home/flo-data /tank/subvol-100-nas-flo
+$SCRIPTDIR/startBackup.sh -p $SCRIPTDIR/borg_passphrase_backup_box1_flo-data.txt -n flo-data -r ssh://$USER-sub1@$USER.your-storagebox.de:23/home/flo-data /tank/subvol-100-nas-flo
 
 $SCRIPTDIR/checkBackups.sh -p $SCRIPTDIR/borg_passphrase_backup_box1_dell-t20.txt -r ssh://$USER-sub1@$USER.your-storagebox.de:23/home/dell-t20
 $SCRIPTDIR/checkBackups.sh -p $SCRIPTDIR/borg_passphrase_backup_box1_flo-data.txt -r ssh://$USER-sub1@$USER.your-storagebox.de:23/home/flo-data
